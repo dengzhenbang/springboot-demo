4cd3a6b74f6811c8a54268d3f77c63278a4e3981
zhengzhenbang <454792941@qq.com>
2023-07-22 07:14:00 +0800

    redis

--- springboot-demo/src/main/java/com/dzb/springbootdemo/test/index.java
+++ springboot-demo/src/main/java/com/dzb/springbootdemo/test/index.java
@@ -1,15 +1,25 @@
 package com.dzb.springbootdemo.test;
 
+import com.dzb.springbootdemo.Utils.RedisUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class index {
+
+
+    @Autowired
+    private RedisUtils redisUtils;
+
     @RequestMapping("/index")
     @ResponseBody
     public String index() {
-        return "hello world!" ;
+        String key = "key";
+        //redisUtils.set(key, "hello world!");
+        redisUtils.set(key, "nimasile", 10);
+        return redisUtils.get(key).toString();
     }
 }
 
